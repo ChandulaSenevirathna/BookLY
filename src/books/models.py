c0a849aac2b06acf095b39db5bf8040dad4e39f6
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-04-24 01:16:06 +0530

    books table created

--- src/books/models.py
+++ src/books/models.py
@@ -19,5 +19,8 @@ class Book(SQLModel, table=True):
     published_date: str
     page_count: int
     language: str
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    
+    def __repr__(self):
+        return f"<Book {self.title} by {self.author}>"
