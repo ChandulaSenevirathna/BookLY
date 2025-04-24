4a07e658c1cafe3941ac999fae75b8af4125f7e7
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-04-24 20:18:25 +0530

    updated services

--- src/books/service.py
+++ src/books/service.py
@@ -1,13 +1,25 @@
 from sqlmodel.ext.asyncio import AsyncSession
+from src.books.schemas import BookCreateModel, BookUpdateModel
+from sqlmodel import select, desc
+from src.books.models import Book
 
 class BookService:
     
     async def get_all_books(self, session: AsyncSession):
-        pass
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.execute(statement)
+        return result.all()
     
     async def get_book(self, book_uid: str, session: AsyncSession):
+        statement = select(Book).where(Book.uid == book_uid)
+        result = await session.execute(statement)
+        return result.scalar_one_or_none()
+    
+    async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
         pass
     
-    async def create_book(self, book_data, session: AsyncSession):
+    async def update_book(self, update_data: BookUpdateModel, session: AsyncSession):
         pass
     
+    async def delete_book(self, book_data: BookCreateModel, session: AsyncSession):
+        pass
