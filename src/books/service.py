5426e9c5dc3d97415dfceab5532adcf2daf264b0
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-04-24 22:19:41 +0530

    updated services

--- src/books/service.py
+++ src/books/service.py
@@ -13,7 +13,7 @@ class BookService:
     async def get_book(self, book_uid: str, session: AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
         result = await session.execute(statement)
-        return result.scalar_one_or_none()
+        return result.first()
     
     async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
         book_data_dict = book_data.model_dump()
@@ -24,7 +24,16 @@ class BookService:
         return new_book
     
     async def update_book(self, update_data: BookUpdateModel, session: AsyncSession):
-        pass
+        
+        book_to_update = await self.get_book(update_data.uid, session)
+        
+        update_data_dict = update_data.model_dump()
+        
+        for key, value in update_data_dict.items():
+            setattr(book_to_update, key, value)
+        await session.commit()
+        await session.refresh(book_to_update)
+        return book_to_update
     
     async def delete_book(self, book_data: BookCreateModel, session: AsyncSession):
         pass
