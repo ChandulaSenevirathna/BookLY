39b618c8e2991cd3e6f058c7f46925e371999ff4
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-04-24 19:02:20 +0530

    new files added

---
+++ src/books/service.py
@@ -0,0 +1,13 @@
+from sqlmodel.ext.asyncio import AsyncSession
+
+class BookService:
+    
+    async def get_all_books(self, session: AsyncSession):
+        pass
+    
+    async def get_book(self, book_uid: str, session: AsyncSession):
+        pass
+    
+    async def create_book(self, book_data, session: AsyncSession):
+        pass
+    
