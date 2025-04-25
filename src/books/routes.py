a0a03ab2f5d9b854aa6d2c0c6f66d2f08409b8cd
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-04-25 17:00:38 +0530

    routes updated

--- src/books/routes.py
+++ src/books/routes.py
@@ -18,39 +18,38 @@ async def get_books(session: AsyncSession = Depends(get_session)):
     return books
 
 @book_router.post("", status_code=status.HTTP_201_CREATED)
-async def create_a_book(book_data: schemas.Book):
-    new_book = book_data.model_dump()
-    books.append(new_book)
+async def create_a_book(book_data: schemas.Book, session: AsyncSession = Depends(get_session)):
+    
+    new_book_data = book_data.model_dump()
+    new_book = await book_service.create_book(new_book_data, session)
     return new_book
 
 @book_router.get("/{book_id}", status_code=status.HTTP_200_OK)
-async def get_book(book_id: int):
-    for book in books:
-        if book["id"] == book_id:
-            return book
-        
-    raise HTTPException(status_code=404, detail="Book not found")
+async def get_book(book_uid: int, session: AsyncSession = Depends(get_session)):
+    
+    book = await book_service.get_book(book_uid, session)
+    
+    if book:
+        return book
+    else:  
+        raise HTTPException(status_code=404, detail="Book not found")
 
-@book_router.put("/{book_id}")
-async def update_book(book_id: int, book_data: schemas.BookUpdateModel):
-    for book in books:
-        if book["id"] == book_id:
-            book.update(book_data.model_dump())
-            return {
-                "status": "success",
-                "record": book
-                }
-    raise HTTPException(status_code=404, detail="Book not found")
+# @book_router.put("/{book_id}")
+# async def update_book(book_id: int, book_data: schemas.BookUpdateModel):
+#     for book in books:
+#         if book["id"] == book_id:
+#             book.update(book_data.model_dump())
+#             return {
+#                 "status": "success",
+#                 "record": book
+#                 }
+#     raise HTTPException(status_code=404, detail="Book not found")
 
 @book_router.patch("/{book_id}")
 async def update_book(book_id: int, book_data: schemas.BookUpdateModelV1):
-    for book in books:
-        if book["id"] == book_id:
-            book["title"] = book_data.title
-            return {
-                "status": "success",
-                "record": book
-                }
+    
+    
+    
     raise HTTPException(status_code=404, detail="Book not found")
 
 @book_router.delete("/{book_id}", status_code=status.HTTP_200_OK)
