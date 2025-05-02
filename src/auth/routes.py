44d40dc07a7bb8a0ea3bd017630aa56189e086e4
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-02 19:56:13 +0530

    user creation done

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,status
 from src.auth.schemas import UserCreateModel
 from src.auth.service import UserService
 from src.db.main import get_session
@@ -14,7 +14,7 @@ async def create_user_account(user_data: UserCreateModel, session=Depends(get_se
     user_exists = await user_service.user_exists(email=email, session=session)
     
     if user_exists == True:
-        return {"message": "User already exists"}
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User already exists")  
     else:
         new_user = await user_service.create_user(user_data=user_data, session=session)
         return {"message": "User created successfully", "user": new_user}
--- src/auth/service.py
+++ src/auth/service.py
@@ -1,7 +1,7 @@
 from flask import session
 from src.auth.models import User
 from src.auth import schemas, utils
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 
 class UserService:
