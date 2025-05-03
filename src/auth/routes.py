5c5c6d02114da5ccb30e68d5cd5316f7546a1a92
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-03 19:04:23 +0530

    user creation update

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException,status
-from src.auth.schemas import UserCreateModel
+from src.auth.schemas import UserCreateModel, UserModel
 from src.auth.service import UserService
 from src.db.main import get_session
 
 auth_router = APIRouter()
 user_service = UserService()
 
-@auth_router.post("/signup")
+@auth_router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=UserModel)
 async def create_user_account(user_data: UserCreateModel, session=Depends(get_session)):
     
     email = user_data.email
@@ -17,4 +17,4 @@ async def create_user_account(user_data: UserCreateModel, session=Depends(get_se
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User already exists")  
     else:
         new_user = await user_service.create_user(user_data=user_data, session=session)
-        return {"message": "User created successfully", "user": new_user}
+        return new_user
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import uuid
 from pydantic import BaseModel, Field
 
 class UserCreateModel(BaseModel):
@@ -6,3 +8,14 @@ class UserCreateModel(BaseModel):
     password: str = Field(min_length=6, max_length=12)
     first_name: str
     last_name: str
+    
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str = Field(max_length=10)
+    email: str = Field(max_length=50)
+    password_hash: str = Field(exclude=True)
+    first_name: str
+    last_name: str
+    is_verified: bool
+    created_at: datetime 
+    updated_at: datetime
