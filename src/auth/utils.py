ce0e4bad124f8b720da60906399ffcc6f742332a
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-04 17:44:17 +0530

    create token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,8 @@
+from aiohttp import Payload
 from passlib.context import CryptContext
+from datetime import timedelta, datetime
+import jwt
+from src.config import config
 
 password_context = CryptContext(
     schemes=['bcrypt']
@@ -15,3 +19,19 @@ def verify_password(plain_password: str, hashed_password: str):
     Verify a plain password against a hashed password.
     """
     return password_context.verify(plain_password, hashed_password)
+
+def create_access_token(user_data: dict, expiry_time: timedelta):
+    """
+    Create an access token for the user.
+    """
+    
+    Payload = {}
+    
+    token = jwt.encode(
+        payload = Payload,
+        key = config.JWT_SECRET_KEY,
+        algorithm = config.JWT_ALGORITHM,
+    )
+    
+    return token
+
--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-
 class Settings(BaseSettings):
     DATABASE_URL: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
     
     model_config = SettingsConfigDict(
         env_file=Path(__file__).resolve().parent/".env",
