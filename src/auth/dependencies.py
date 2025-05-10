1a2cbe60625c4d2e3fff2fd4fca5458082452572
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-10 18:04:54 +0530

    token validation

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,7 +1,8 @@
 from email.policy import HTTP
-from fastapi import HTTPException, Request
+from fastapi import HTTPException, Request, status
 from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
+from src.auth.utils import decode_token
 
 class AccessTokenBearer(HTTPBearer):
     
@@ -11,4 +12,25 @@ class AccessTokenBearer(HTTPBearer):
     async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
         creds = await super().__call__(request)
         
-        print(creds.scheme, creds.credentials)
+        # print(creds.scheme, creds.credentials)
+        
+        token = creds.credentials
+        
+        is_token_valid = self.token_valid(token)
+        
+        if is_token_valid == False:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid or expired token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    
+    def token_valid(self, token: str):
+        
+        token_data = decode_token(token)
+        
+        if token_data is not None:
+            return True
+        else:
+            return False
+        
