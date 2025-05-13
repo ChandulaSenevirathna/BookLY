44430ed478949e725fdd94f21d4a5c32e4c34795
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-13 17:51:35 +0530

    jti blocklist

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,8 +1,18 @@
 import aioredis
 from src.config import config
 
+JTI_EXPIRY = 3600  # 1 hour
+
 token_blocklist = aioredis.StrictRedis(
     host=config.REDIS_HOST,
     port=config.REDIS_PORT,
     db=0
 )
+
+async def add_jti_to_blocklist(jti: str):
+    
+    await token_blocklist.set(
+        name=jti,
+        value="",
+        ex=JTI_EXPIRY
+    )
