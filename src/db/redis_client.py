92fda158299d6cbf9f34f881b6c0602d02281aa0
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-14 13:58:23 +0530

    redis code update

--- src/db/redis_client.py
+++ src/db/redis_client.py
@@ -1,9 +1,10 @@
-import aioredis
+import asyncio
+import redis.asyncio as redis
 from src.config import config
 
 JTI_EXPIRY = 3600  # 1 hour
 
-token_blocklist = aioredis.StrictRedis(
+token_blocklist = redis.Redis(
     host=config.REDIS_HOST,
     port=config.REDIS_PORT,
     db=0
@@ -16,3 +17,12 @@ async def add_jti_to_blocklist(jti: str):
         value="",
         ex=JTI_EXPIRY
     )
+    
+async def token_in_blocklist(jti: str) -> bool:
+    
+    exists = await token_blocklist.exists(jti)
+    
+    if exists:
+        return True
+    
+    return False
