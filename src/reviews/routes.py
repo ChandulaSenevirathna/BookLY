257fdefde870a782b6c6ac90c4ab1e4cd5bc8899
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-22 19:15:43 +0530

    add review service complete

--- src/reviews/routes.py
+++ src/reviews/routes.py
@@ -1,12 +1,14 @@
-import re
 from fastapi import APIRouter, Depends, HTTPException
 from src.reviews.service import ReviewService
 from src.reviews.schemas import ReviewCreateModel
+from src.db.models import User
+from src.auth.dependencies import current_user
 
 reviews_router = APIRouter()
 review_service = ReviewService()
 
-@reviews_router.get("/")
-async def get_reviews(review_data: ReviewCreateModel):
+@reviews_router.post("/book/{book_uid}")
+async def add_review_to_books(book_uid: str, review_data: ReviewCreateModel,
+                              current_user: User = Depends(current_user)):
     
-    pass
+    print(current_user)
--- src/reviews/service.py
+++ src/reviews/service.py
@@ -5,12 +5,29 @@ from src.reviews.schemas import ReviewCreateModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi import HTTPException, status
 
+book_service = BookService()
+user_service = UserService()
+
 class ReviewService:
     
     async def add_review_to_book(self, user_email:str, book_uid:str, review_data:ReviewCreateModel,
                                  session:AsyncSession):
         
         try:
-            pass
+            book = await book_service.get_book(book_uid=book_uid, session=session)
+            user = await user_service.get_user(email=user_email, session=session)
+            
+            new_review = Review(
+                rating=review_data.rating,
+                review=review_data.review,
+                user_uid=user.uid,
+                book_uid=book.uid
+            )
+            
+            session.add(new_review)
+            await session.commit()
+            
+            return new_review
+            
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Oops something went wrong")
