64c872214e42f6ccf02323ef92977325326b2c28
ChandulaSenevirathna <chandulasenevirathna@gmail.com>
2025-05-22 20:10:01 +0530

    add review route complete

--- src/reviews/routes.py
+++ src/reviews/routes.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
+from src.db.main import get_session
 from src.reviews.service import ReviewService
 from src.reviews.schemas import ReviewCreateModel
 from src.db.models import User
 from src.auth.dependencies import current_user
+from sqlalchemy.ext.asyncio import AsyncSession
 
 reviews_router = APIRouter()
 review_service = ReviewService()
 
 @reviews_router.post("/book/{book_uid}")
 async def add_review_to_books(book_uid: str, review_data: ReviewCreateModel,
-                              current_user: User = Depends(current_user)):
+                              current_user: User = Depends(current_user),
+                              session: AsyncSession = Depends(get_session)):
     
-    print(current_user)
+    
+    new_review = await review_service.add_review_to_book(
+        user_email=current_user.email,
+        book_uid=book_uid,
+        review_data=review_data,
+        session=session
+    )
+    
+    return new_review
--- src/reviews/service.py
+++ src/reviews/service.py
@@ -10,12 +10,18 @@ user_service = UserService()
 
 class ReviewService:
     
-    async def add_review_to_book(self, user_email:str, book_uid:str, review_data:ReviewCreateModel,
-                                 session:AsyncSession):
-        
+    async def add_review_to_book(self, user_email: str, book_uid: str, review_data: ReviewCreateModel,
+                                 session: AsyncSession):
         try:
             book = await book_service.get_book(book_uid=book_uid, session=session)
+            
+            if book is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+            
             user = await user_service.get_user(email=user_email, session=session)
+            
+            if user is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
             
             new_review = Review(
                 rating=review_data.rating,
@@ -29,5 +35,8 @@ class ReviewService:
             
             return new_review
             
+        except HTTPException as http_exc:
+            raise http_exc  
+        
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Oops something went wrong")
